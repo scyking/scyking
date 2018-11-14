6be74b11f844c320ed7e19b21360b396dd351053
scyking <scyking@hotmail.com>
2018-11-14 11:53:41 +0800

    更改项目为Maven多模块结构
    1. 添加web模块
    2. 添加ucenter模块（统一用户管理模块）

--- web/src/main/java/com/example/demo/DemoApplication.java
+++ web/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootdemo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
