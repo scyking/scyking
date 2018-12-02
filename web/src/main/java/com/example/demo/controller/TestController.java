50e56bc683c2cb4d3140771847eb92b0e9e7bcf7
scyking <scyking@hotmail.com>
2018-12-02 15:02:42 +0800

    新增文件下载测试方法

--- web/src/main/java/com/example/demo/controller/TestController.java
+++ web/src/main/java/com/example/demo/controller/TestController.java
@@ -7,6 +7,9 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
 @Api(value = "TestController", tags = {"测试控制器"})
 @RestController
 @RequestMapping("/test")
@@ -19,5 +22,42 @@ public class TestController {
     @GetMapping("/{name}")
     public Object helloWorld(@PathVariable String name, @RequestParam("id") String id) {
         return name + id;
+    }
+
+    @GetMapping("/file/download")
+    public void fileDownload(HttpServletResponse response) {
+        String filename = "三网手机实名制认证.pdf";
+        String filePath = "E:\\file\\人资相关\\手机三要素认证";
+        File file = new File(filePath + "/" + filename);
+        if (file.exists()) { //判断文件父目录是否存在
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+            response.setContentType("application/octet-stream;charset=UTF-8");
+
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("----------file download" + filename);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
