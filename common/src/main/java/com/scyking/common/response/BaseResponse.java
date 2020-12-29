346b13394923105a6b18e306bbe0b2af1e1a62b0
scyking <scyking@hotmail.com>
2020-12-29 17:06:55 +0800

    添加response

---
+++ common/src/main/java/com/scyking/common/response/BaseResponse.java
@@ -0,0 +1,15 @@
+package com.scyking.common.response;
+
+import lombok.Data;
+
+/**
+ * @author scyking
+ * @description
+ * @createTime 2020/12/29 16:50
+ **/
+@Data
+public class BaseResponse<T> {
+    private Integer code;
+    private String msg;
+    private T data;
+}
