1e9bec6b81c928e2ec3393e665af6d6da1a71e2e
scyking <scyking@hotmail.com>
2020-12-30 14:35:03 +0800

    添加跨域配置

---
+++ u-center/src/main/java/com/scyking/ucenter/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.scyking.ucenter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author scyking
+ * @description 跨域配置
+ **/
+@Configuration
+public class CorsConfig {
+
+    /**
+     * 跨越配置
+     * 改用过滤器CorsFilter来配置跨域，由于Filter的位置是在Interceptor之前的，拦截器获取不到自定义header的问题得到解决：
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("token");
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(configSource);
+    }
+}
