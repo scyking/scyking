fda83e327c71e26acf7109db5d3962f5ac75a95d
scyking <scyking@hotmail.com>
2020-12-30 16:44:47 +0800

    添加mysql初始化配置

---
+++ u-center/src/main/resources/db/schema.sql
@@ -0,0 +1,146 @@
+-- 菜单
+CREATE TABLE IF NOT EXISTS `sys_menu` (
+  `menu_id` bigint NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint COMMENT '父菜单ID，一级菜单为0',
+  `name` varchar(50) COMMENT '菜单名称',
+  `url` varchar(200) COMMENT '菜单URL',
+  `perms` varchar(500) COMMENT '授权(多个用逗号分隔，如：user:list,user:create)',
+  `type` int COMMENT '类型   0：目录   1：菜单   2：按钮',
+  `icon` varchar(50) COMMENT '菜单图标',
+  `order_num` int COMMENT '排序',
+  PRIMARY KEY (`menu_id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='菜单管理';
+
+-- 系统用户
+CREATE TABLE IF NOT EXISTS `sys_user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(100) COMMENT '密码',
+  `salt` varchar(20) COMMENT '盐',
+  `email` varchar(100) COMMENT '邮箱',
+  `mobile` varchar(100) COMMENT '手机号',
+  `status` tinyint COMMENT '状态  0：禁用   1：正常',
+  `create_user_id` bigint(20) COMMENT '创建者ID',
+  `create_time` datetime COMMENT '创建时间',
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX (`username`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='系统用户';
+
+-- 系统用户Token
+CREATE TABLE IF NOT EXISTS `sys_user_token` (
+  `user_id` bigint(20) NOT NULL,
+  `token` varchar(100) NOT NULL COMMENT 'token',
+  `expire_time` datetime DEFAULT NULL COMMENT '过期时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='系统用户Token';
+
+-- 系统验证码
+CREATE TABLE IF NOT EXISTS `sys_captcha` (
+  `uuid` char(36) NOT NULL COMMENT 'uuid',
+  `code` varchar(6) NOT NULL COMMENT '验证码',
+  `expire_time` datetime DEFAULT NULL COMMENT '过期时间',
+  PRIMARY KEY (`uuid`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='系统验证码';
+
+-- 角色
+CREATE TABLE IF NOT EXISTS `sys_role` (
+  `role_id` bigint NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(100) COMMENT '角色名称',
+  `remark` varchar(100) COMMENT '备注',
+  `create_user_id` bigint(20) COMMENT '创建者ID',
+  `create_time` datetime COMMENT '创建时间',
+  PRIMARY KEY (`role_id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='角色';
+
+-- 用户与角色对应关系
+CREATE TABLE IF NOT EXISTS `sys_user_role` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint COMMENT '用户ID',
+  `role_id` bigint COMMENT '角色ID',
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='用户与角色对应关系';
+
+-- 角色与菜单对应关系
+CREATE TABLE IF NOT EXISTS `sys_role_menu` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `role_id` bigint COMMENT '角色ID',
+  `menu_id` bigint COMMENT '菜单ID',
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='角色与菜单对应关系';
+
+-- 系统配置信息
+CREATE TABLE IF NOT EXISTS `sys_config` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`param_key` varchar(50) COMMENT 'key',
+	`param_value` varchar(2000) COMMENT 'value',
+	`status` tinyint DEFAULT 1 COMMENT '状态   0：隐藏   1：显示',
+	`remark` varchar(500) COMMENT '备注',
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX (`param_key`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='系统配置信息表';
+
+
+-- 系统日志
+CREATE TABLE IF NOT EXISTS `sys_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COMMENT '用户名',
+  `operation` varchar(50) COMMENT '用户操作',
+  `method` varchar(200) COMMENT '请求方法',
+  `params` varchar(5000) COMMENT '请求参数',
+  `time` bigint NOT NULL COMMENT '执行时长(毫秒)',
+  `ip` varchar(64) COMMENT 'IP地址',
+  `create_date` datetime COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='系统日志';
+
+
+-- 文件上传
+CREATE TABLE IF NOT EXISTS `sys_oss` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `url` varchar(200) COMMENT 'URL地址',
+  `create_date` datetime COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='文件上传';
+
+
+-- 定时任务
+CREATE TABLE IF NOT EXISTS `schedule_job` (
+  `job_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '任务id',
+  `bean_name` varchar(200) DEFAULT NULL COMMENT 'spring bean名称',
+  `params` varchar(2000) DEFAULT NULL COMMENT '参数',
+  `cron_expression` varchar(100) DEFAULT NULL COMMENT 'cron表达式',
+  `status` tinyint(4) DEFAULT NULL COMMENT '任务状态  0：正常  1：暂停',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`job_id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='定时任务';
+
+-- 定时任务日志
+CREATE TABLE IF NOT EXISTS `schedule_job_log` (
+  `log_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '任务日志id',
+  `job_id` bigint(20) NOT NULL COMMENT '任务id',
+  `bean_name` varchar(200) DEFAULT NULL COMMENT 'spring bean名称',
+  `params` varchar(2000) DEFAULT NULL COMMENT '参数',
+  `status` tinyint(4) NOT NULL COMMENT '任务状态    0：成功    1：失败',
+  `error` varchar(2000) DEFAULT NULL COMMENT '失败信息',
+  `times` int(11) NOT NULL COMMENT '耗时(单位：毫秒)',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`log_id`),
+  KEY `job_id` (`job_id`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='定时任务日志';
+
+
+
+-- 用户表
+CREATE TABLE IF NOT EXISTS `tb_user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `mobile` varchar(20) NOT NULL COMMENT '手机号',
+  `password` varchar(64) COMMENT '密码',
+  `create_time` datetime COMMENT '创建时间',
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX (`username`)
+) ENGINE=`InnoDB` DEFAULT CHARACTER SET utf8mb4 COMMENT='用户';
+
