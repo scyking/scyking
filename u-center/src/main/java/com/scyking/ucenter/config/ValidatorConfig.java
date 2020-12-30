aa6b7393e02847491581835e9ec5732fc161916d
scyking <scyking@hotmail.com>
2020-12-30 14:35:22 +0800

    添加校验器配置

---
+++ u-center/src/main/java/com/scyking/ucenter/config/ValidatorConfig.java
@@ -0,0 +1,25 @@
+package com.scyking.ucenter.config;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+/**
+ * @author scyking
+ * @description 校验器配置
+ **/
+public class ValidatorConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .failFast(true)
+                .buildValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        return validator;
+    }
+}
