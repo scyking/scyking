dc91e6ecca62532bb5e962da6e4617f523d2814f
scyking <scyking@hotmail.com>
2021-01-04 18:11:15 +0800

    修改BaseResponse

--- common/src/main/java/com/scyking/common/responses/BaseResponse.java
+++ common/src/main/java/com/scyking/common/responses/BaseResponse.java
@@ -4,12 +4,52 @@ import lombok.Data;
 
 /**
  * @author scyking
- * @description
+ * @description 链式返回结果
  * @createTime 2020/12/29 16:50
  **/
 @Data
 public class BaseResponse<T> {
-    private Integer code;
+    public static final int SUCCESS = 200;
+    public static final int ERROR = 500;
+
+    private int code;
     private String msg;
     private T data;
+
+    private BaseResponse(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static BaseResponse success() {
+        return new BaseResponse<>(BaseResponse.SUCCESS, "操作成功！", null);
+    }
+
+    public static BaseResponse error() {
+        return new BaseResponse<>(BaseResponse.ERROR, "操作失败！", null);
+    }
+
+    public BaseResponse msg(String message) {
+        this.msg = message;
+        return this;
+    }
+
+    public BaseResponse data(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public BaseResponse code(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public boolean isOk() {
+        return this.code == SUCCESS;
+    }
+
+    public boolean isError() {
+        return !isOk();
+    }
 }
