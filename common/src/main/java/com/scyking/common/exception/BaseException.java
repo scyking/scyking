67bea6c90df3cfeec6112c843d2dc1e4d1b262d4
scyking <scyking@hotmail.com>
2021-01-06 17:56:25 +0800

    添加BaseException

---
+++ common/src/main/java/com/scyking/common/exception/BaseException.java
@@ -0,0 +1,30 @@
+package com.scyking.common.exception;
+
+import lombok.Data;
+
+/**
+ * @author scyking
+ * @description
+ **/
+@Data
+public class BaseException extends RuntimeException {
+    private String msg;
+    private int code = 500;
+
+    public BaseException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public BaseException(String msg, int code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+
+    public BaseException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
+}
