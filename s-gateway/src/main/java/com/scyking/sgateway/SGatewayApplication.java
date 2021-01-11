7e7319f4beb4d2324fe5d34c4455f9055cbb73f3
scyking <scyking@hotmail.com>
2021-01-11 18:16:21 +0800

    统一在网关添加跨域配置

---
+++ s-gateway/src/main/java/com/scyking/sgateway/SGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.scyking.sgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SGatewayApplication.class, args);
+    }
+
+}
--- u-center/src/main/java/com/scyking/ucenter/config/CorsConfig.java
+++ u-center/src/main/java/com/scyking/ucenter/config/CorsConfig.java
@@ -10,14 +10,15 @@ import org.springframework.web.filter.CorsFilter;
  * @author scyking
  * @description 跨域配置
  **/
-@Configuration
+//@Configuration
 public class CorsConfig {
 
     /**
      * 跨越配置
      * 改用过滤器CorsFilter来配置跨域，由于Filter的位置是在Interceptor之前的，拦截器获取不到自定义header的问题得到解决：
      */
-    @Bean
+    //统一在网关添加跨域配置
+    //@Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("*");
