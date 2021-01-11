7f75323275f3ba01781c65786c116a9b6007d402
scyking <scyking@hotmail.com>
2021-01-11 18:22:17 +0800

    添加服务发现 添加环境分类配置

--- s-gateway/src/main/java/com/scyking/sgateway/SGatewayApplication.java
+++ s-gateway/src/main/java/com/scyking/sgateway/SGatewayApplication.java
@@ -2,7 +2,9 @@ package com.scyking.sgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SGatewayApplication {
 
