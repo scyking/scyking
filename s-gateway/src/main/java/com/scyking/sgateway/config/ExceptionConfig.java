319ddb17cf2c6c3fd483e6da4200ce52358ea3ff
scyking <scyking@hotmail.com>
2021-01-11 18:26:23 +0800

    添加网关统一异常处理

---
+++ s-gateway/src/main/java/com/scyking/sgateway/config/ExceptionConfig.java
@@ -0,0 +1,38 @@
+package com.scyking.sgateway.config;
+
+import com.scyking.sgateway.exception.JsonExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author scyking
+ * @description
+ **/
+@Configuration
+public class ExceptionConfig {
+
+    /**
+     * 自定义异常处理
+     */
+    @Primary
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public ErrorWebExceptionHandler errorWebExceptionHandler(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                                             ServerCodecConfigurer serverCodecConfigurer) {
+        JsonExceptionHandler jsonExceptionHandler = new JsonExceptionHandler();
+        jsonExceptionHandler.setViewResolvers(viewResolversProvider.getIfAvailable(Collections::emptyList));
+        jsonExceptionHandler.setMessageWriters(serverCodecConfigurer.getWriters());
+        jsonExceptionHandler.setMessageReaders(serverCodecConfigurer.getReaders());
+        return jsonExceptionHandler;
+    }
+}
