56a49fffa81846b4122f79b732148c309fdb0a6f
scyking <scyking@hotmail.com>
2021-01-12 17:29:48 +0800

    统一异常修改

--- s-gateway/src/main/java/com/scyking/sgateway/exception/JsonExceptionHandler.java
+++ s-gateway/src/main/java/com/scyking/sgateway/exception/JsonExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.scyking.sgateway.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.scyking.common.responses.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,109 +20,38 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author scyking
  * @description 网关统一异常处理
  **/
+@Slf4j
 public class JsonExceptionHandler implements ErrorWebExceptionHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(JsonExceptionHandler.class);
-
-    /**
-     * MessageReader
-     */
     private List<HttpMessageReader<?>> messageReaders = Collections.emptyList();
-
-    /**
-     * MessageWriter
-     */
     private List<HttpMessageWriter<?>> messageWriters = Collections.emptyList();
-
-    /**
-     * ViewResolvers
-     */
     private List<ViewResolver> viewResolvers = Collections.emptyList();
+    // 存储处理异常后的信息
+    private ThreadLocal<BaseResponse> exceptionHandlerResult = new ThreadLocal<>();
 
-    /**
-     * 存储处理异常后的信息
-     */
-    private ThreadLocal<Map<String, Object>> exceptionHandlerResult = new ThreadLocal<>();
-
-    /**
-     * 参考AbstractErrorWebExceptionHandler
-     */
+    //*******************************************************************************/
+    //*****************   参考AbstractErrorWebExceptionHandler   ********************/
+    //*******************************************************************************/
     public void setMessageReaders(List<HttpMessageReader<?>> messageReaders) {
         Assert.notNull(messageReaders, "'messageReaders' must not be null");
         this.messageReaders = messageReaders;
     }
 
-    /**
-     * 参考AbstractErrorWebExceptionHandler
-     */
     public void setViewResolvers(List<ViewResolver> viewResolvers) {
         this.viewResolvers = viewResolvers;
     }
 
-    /**
-     * 参考AbstractErrorWebExceptionHandler
-     */
     public void setMessageWriters(List<HttpMessageWriter<?>> messageWriters) {
         Assert.notNull(messageWriters, "'messageWriters' must not be null");
         this.messageWriters = messageWriters;
     }
 
-    @Override
-    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
-        // 按照异常类型进行处理
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        String message = "网关内部错误！";
-        if (ex instanceof ResponseStatusException) {
-            ResponseStatusException responseStatusException = (ResponseStatusException) ex;
-            httpStatus = responseStatusException.getStatus();
-            message = responseStatusException.getMessage();
-        }
-        if(ex instanceof IllegalArgumentException){
-            message = ex.getMessage();
-        }
-        // 封装响应体,此body可修改为自己的jsonBody
-        Map<String, Object> result = new HashMap<>(2, 1);
-        result.put("httpStatus", httpStatus);
-
-        String msg = "{\"code\":" + httpStatus.value() + ",\"msg\": \"" + message + "\"}";
-        result.put("body", msg);
-        // 错误记录
-        ServerHttpRequest request = exchange.getRequest();
-        log.error("[全局异常处理]异常请求路径:{},记录异常信息:{}", request.getPath(), ex.getMessage());
-        // 参考AbstractErrorWebExceptionHandler
-        if (exchange.getResponse().isCommitted()) {
-            return Mono.error(ex);
-        }
-        exceptionHandlerResult.set(result);
-        ServerRequest newRequest = ServerRequest.create(exchange, this.messageReaders);
-        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse).route(newRequest)
-                .switchIfEmpty(Mono.error(ex))
-                .flatMap((handler) -> handler.handle(newRequest))
-                .flatMap((response) -> write(exchange, response));
-
-    }
-
-    /**
-     * 参考DefaultErrorWebExceptionHandler
-     */
-    protected Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
-        Map<String, Object> result = exceptionHandlerResult.get();
-        return ServerResponse.status((HttpStatus) result.get("httpStatus"))
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .body(BodyInserters.fromObject(result.get("body")));
-    }
-
-    /**
-     * 参考AbstractErrorWebExceptionHandler
-     */
     private Mono<? extends Void> write(ServerWebExchange exchange,
                                        ServerResponse response) {
         exchange.getResponse().getHeaders()
@@ -130,9 +59,6 @@ public class JsonExceptionHandler implements ErrorWebExceptionHandler {
         return response.writeTo(exchange, new ResponseContext());
     }
 
-    /**
-     * 参考AbstractErrorWebExceptionHandler
-     */
     private class ResponseContext implements ServerResponse.Context {
 
         @Override
@@ -145,4 +71,44 @@ public class JsonExceptionHandler implements ErrorWebExceptionHandler {
             return JsonExceptionHandler.this.viewResolvers;
         }
     }
+    //*******************************************************************************/
+    //*****************   参考AbstractErrorWebExceptionHandler   ********************/
+    //*******************************************************************************/
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        // 统一异常拦截
+        int code = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        String msg = "网关内部错误！";
+        if (ex instanceof ResponseStatusException) {
+            ResponseStatusException responseStatusException = (ResponseStatusException) ex;
+            code = responseStatusException.getStatus().value();
+            msg = responseStatusException.getMessage();
+        }
+        if (ex instanceof IllegalArgumentException) {
+            msg = ex.getMessage();
+        }
+
+
+        // 错误记录
+        ServerHttpRequest request = exchange.getRequest();
+        log.error("[全局异常处理]异常请求路径:{},记录异常信息:{}", request.getPath(), ex.getMessage());
+        if (exchange.getResponse().isCommitted()) {
+            return Mono.error(ex);
+        }
+        exceptionHandlerResult.set(BaseResponse.error().code(code).msg(msg));
+        ServerRequest newRequest = ServerRequest.create(exchange, this.messageReaders);
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse).route(newRequest)
+                .switchIfEmpty(Mono.error(ex))
+                .flatMap((handler) -> handler.handle(newRequest))
+                .flatMap((response) -> write(exchange, response));
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        BaseResponse result = exceptionHandlerResult.get();
+        return ServerResponse.status(HttpStatus.valueOf(result.getCode()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(result));
+    }
+
 }
