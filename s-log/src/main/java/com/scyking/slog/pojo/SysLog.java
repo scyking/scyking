16d11f5eb283d2797cd6990752c2822502200094
scyking <scyking@hotmail.com>
2021-02-01 14:40:20 +0800

    修改实体属性名为头峰形式

--- s-log/src/main/java/com/scyking/slog/pojo/SysLog.java
+++ s-log/src/main/java/com/scyking/slog/pojo/SysLog.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 /**
  * @author scyking
@@ -17,16 +17,16 @@ import java.util.Date;
 public class SysLog<T> {
     @Id
     private String id;
-    private String opt_id;                          //操作用户id
-    private String opt_account;                     //操作人账号
-    private String opt_name;                        //操作人name
+    private String optId;                          //操作用户id
+    private String optAccount;                     //操作人账号
+    private String optName;                        //操作人name
 
     private T obj;                                  //操作主体
-    private SystemLogModuleEnum log_type;           //日志类型
+    private SystemLogModuleEnum logType;           //日志类型
 
-    private SystemLogTypeEnum opt_type;            //操作类型
+    private SystemLogTypeEnum optType;            //操作类型
     private String remark;                          //备注
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS", timezone = "GMT+8")
-    private Date create_at;             //记录时间
+    private LocalDateTime createTime;                    //记录时间
 }
